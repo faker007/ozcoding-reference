549ea11d3d30761763cf2a5761d21ded1b03664c
faker007 <rlaqjadud195@naver.com>
2024-09-11 11:53:06 +0900

    add: Day 15 Reference

---
+++ day15/reference/index.js
@@ -0,0 +1,10 @@
+// 1. 객체 생성하기 (난이도: 쉬움)
+const person = {
+  이름: "홍길동",
+  나이: 254,
+};
+
+// 2. 객체 속성 접근하기 (난이도: 쉬움)
+const car = { brand: "Tesla", model: "Model S" };
+console.log(`자동차 브랜드: ${car.brand}`);
+console.log(`자동차 모델: ${car.model}`);
