a0061ef44e6262320f5517a05e6ff9101f1d1325
faker007 <rlaqjadud195@naver.com>
2024-09-27 11:54:38 +0900

    add: Day 24 practices - hoisting

---
+++ day24/practices/hoisting.js
@@ -0,0 +1,14 @@
+console.log(x); // undefined
+console.log(y);
+
+var x = 5;
+let y = 10;
+
+console.log(x); // 5
+console.log(y);
+
+sayHello();
+
+function sayHello() {
+  console.log("Hello, World!");
+}
