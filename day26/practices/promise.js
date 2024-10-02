58bbc7258ccd542f209f4c8fd423493213651086
faker007 <rlaqjadud195@naver.com>
2024-10-02 10:45:47 +0900

    add: promise.js

---
+++ day26/practices/promise.js
@@ -0,0 +1,13 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Success!");
+  }, 1000);
+});
+
+promise
+  .then((message) => {
+    console.log(message); // 'Success!'
+  })
+  .catch((error) => {
+    console.error(error);
+  });
