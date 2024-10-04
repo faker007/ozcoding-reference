ace02cc41b5db9e334512e3e1c40c1aaa9f3c169
faker007 <rlaqjadud195@naver.com>
2024-10-04 10:25:16 +0900

    add: class.js - 2

--- day27/practices/class.js
+++ day27/practices/class.js
@@ -16,3 +16,20 @@ class Car {
 const myCar = new Car("Hyundai", "Sonata", 2020);
 
 myCar.startEngine();
+
+class 붕어빵_틀 {
+  constructor(맛, 가격) {
+    this.맛 = 맛;
+    this.가격 = 가격;
+  }
+
+  sell() {
+    console.log(`${this.맛} 붕어빵이 ${this.가격}원에 팔렸습니다.`);
+  }
+}
+
+const 팥_붕어빵 = new 붕어빵_틀("팥", 1000);
+const 슈크림_붕어빵 = new 붕어빵_틀("슈크림", 1200);
+
+팥_붕어빵.sell();
+슈크림_붕어빵.sell();
