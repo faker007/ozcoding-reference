866904644a285d94325187e796f02ac8b161a4d6
faker007 <rlaqjadud195@naver.com>
2024-10-17 11:09:35 +0900

    add: httpServer.js

--- day35/practices/httpServer.js
+++ day35/practices/httpServer.js
@@ -1,1 +1,26 @@
 const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    // 비동기 작업 -> 동기 작업 보다 높은 동시성 구현 가능.
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end("서버 에러");
+
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("페이지를 찾을 수 없습니다.");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("서버가 포트 3000에서 실행 중입니다.");
+});
