d2e186831f5cc7ccece42c560823efc176a66e4f
faker007 <rlaqjadud195@naver.com>
2024-10-22 10:29:30 +0900

    add: routings and request.http

--- day38/practices/index.js
+++ day38/practices/index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.post("/submit", (req, res) => {
+  res.send("Form submitted");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
