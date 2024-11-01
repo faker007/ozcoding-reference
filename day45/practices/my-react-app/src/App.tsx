314c821f231ae453a8fa46ebe159da882c3fd0b9
faker007 <rlaqjadud195@naver.com>
2024-11-01 11:00:55 +0900

    add: <Greeting />

--- day45/practices/my-react-app/src/App.tsx
+++ day45/practices/my-react-app/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Greeting from "./components/Greeting";
 
 function App() {
   return (
     <>
       <Counter />
+
+      <Greeting name="홍길동" />
+      <Greeting name="김철수" />
+      <Greeting name="이영희" />
     </>
   );
 }
---
+++ day45/practices/my-react-app/src/components/Greeting.tsx
@@ -0,0 +1,5 @@
+const Greeting = (props) => {
+  return <h2>안녕하세요, {props.name}님!</h2>;
+};
+
+export default Greeting;
